9255e74c5e7c48df90dcd0c52b6830d4d4c9b89d
Farischa Makay <farischamakay09@gmail.com>
2022-07-16 15:48:18 +0700

    Final Application push to github.

---
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskViewModel.kt
@@ -0,0 +1,30 @@
+package com.dicoding.todoapp.ui.detail
+
+import androidx.lifecycle.*
+import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class DetailTaskViewModel(private val taskRepository: TaskRepository): ViewModel() {
+
+    private val _taskId = MutableLiveData<Int>()
+
+    private val _task = _taskId.switchMap { id ->
+        taskRepository.getTaskById(id)
+    }
+    val task: LiveData<Task> = _task
+
+    fun setTaskId(taskId: Int) : LiveData<Task>{
+            return taskRepository.getTaskById(taskId = taskId)
+    }
+
+
+    fun deleteTask(task : Task) {
+        viewModelScope.launch(Dispatchers.IO) {
+            taskRepository.deleteTask(task)
+        }
+    }
+
+
+}
